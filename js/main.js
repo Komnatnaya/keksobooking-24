a1d435d79ec7a6df9b1990b530721f9bfcf40d32
Евгений Кулаков <bseugene@ya.ru>
2021-09-25 11:35:11 +0300

    Добавит функцию возвращающую случайное флоат число

--- js/main.js
+++ js/main.js
@@ -8,4 +8,19 @@ const getRandomInteger = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
+const getRandomFloat = (min, max, floatValue) => {
+  if (
+    min > max ||
+    min < MIN_VALUE ||
+    max < MIN_VALUE ||
+    floatValue <= MIN_VALUE
+  ) {
+    return null;
+  }
+
+  const randValue = Math.random() * (max - min) + min;
+  return Number(randValue.toFixed(floatValue));
+};
+
 getRandomInteger(0, 42);
+getRandomFloat(1.2, 1.4, 3);
