797bf86881bdf831c0e194d4d193bd87c61170ed
Keks <keks@htmlacademy.ru>
2021-10-03 11:15:28 +0300

    :heavy_check_mark: Сборка #3

---
+++ 3/js/utils/get-random-positive-integer.js
@@ -0,0 +1,7 @@
+export const getRandomPositiveInteger = (alpha, beta) => {
+  const lower = Math.ceil(Math.min(Math.abs(alpha), Math.abs(beta)));
+  const upper = Math.floor(Math.max(Math.abs(alpha), Math.abs(beta)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+
+  return Math.floor(result);
+};
