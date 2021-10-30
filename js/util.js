0c48ff131d39638a243945fcb7f61f4a4d6274fd
Евгений Кулаков <bseugene@ya.ru>
2021-10-30 14:18:41 +0300

    Удалит неиспользуемые функции

--- js/util.js
+++ js/util.js
@@ -8,22 +8,6 @@ const errorNode = document.querySelector('#error')
 
 const container = document.querySelector('body');
 
-export const getRandomPositiveInteger = (alpha, beta) => {
-  const lower = Math.ceil(Math.min(Math.abs(alpha), Math.abs(beta)));
-  const upper = Math.floor(Math.max(Math.abs(alpha), Math.abs(beta)));
-  const result = Math.random() * (upper - lower + 1) + lower;
-
-  return Math.floor(result);
-};
-
-export const getRandomPositiveFloat = (alpha, beta, digits = 1) => {
-  const lower = Math.min(Math.abs(alpha), Math.abs(beta));
-  const upper = Math.max(Math.abs(alpha), Math.abs(beta));
-  const result = Math.random() * (upper - lower) + lower;
-
-  return result.toFixed(digits);
-};
-
 export const getDeclension = (number, titles) => {
   const cases = [2, 0, 1, 1, 1, 2];
   return titles[ (number % 100 > 4 && number % 100 < 20) ? 2 : cases[(number % 10 < 5) ? number % 10 : 5] ];
